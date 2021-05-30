5565b054915af697a1d594c7a36d87e660347369
ibizaSunrise <66250856+ibizaSunrise@users.noreply.github.com>
2021-05-30 06:59:28 +0100

    Add files via upload

---
+++ 22.02.js
@@ -0,0 +1,8 @@
+function solution(input, markers) {
+    let lines = input.split("\n");
+    for (var i = 0; i < lines.length; ++i)
+        for (var j = 0; j < markers.length; ++j)
+            lines[i] = lines[i].split(markers[j])[0].trim();
+
+    return lines.join("\n");
+};
